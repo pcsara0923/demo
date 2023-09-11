58121accef13f07cd80c10ef975e3d6cb0817f99
pcsara0923 <130861813+pcsara0923@users.noreply.github.com>
2023-09-11 05:56:36 +0000

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,51 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref,computed } from 'vue'
+
+//給每個todo一個id
+let id = 0
+
+const newTodoList = ref('')
+const newTimeList = ref('')
+const hideCompleted = ref(false)
+
+const todos = ref([
+  { uid: id++, text: 'coding',time:'8:00AM', done: true },
+  { uid: id++, text: 'debug',time:'9:00AM', done: false  }
+])
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value
+    ? todos.value.filter((t) => !t.done)
+    : todos.value
+})
+   
+function addTodo() {
+  todos.value.push({ uid: id++, text: newTodoList.value,time: newTimeList.value, done: false })
+  
+  newTodoList.value = ''
+  newTimeList.value =''
+  done=''
+}
+
+function removeTodo(todo) {
+ todos.value =  todos.value.filter((i) => i !== todo)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTimeList" placeholder="時間"><br>
+    <input v-model="newTodoList" placeholder="事情">
+    <button>新增項目</button>    
+  </form>
+  <ol>
+    <li v-for="todo in filteredTodos" :key="todo.uid">
+     <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.time }} {{todo.text}}</span>
+      <button @click="removeTodo(todo)" style="color:red">x</button>
+    </li>
+  </ol>
+   <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? '顯示全部' : '隱藏完成選項' }}
+  </button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
